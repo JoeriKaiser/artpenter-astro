ca782c0ab715ae37bd058bb6f174b55e37d95e4f
JoeriKaiser <joeri.kaiser@hotmail.com>
2023-03-19 16:24:29 +0100

    wip

--- front/src/utils/constants.ts
+++ front/src/utils/constants.ts
@@ -1,13 +1,11 @@
-export const API_URL = 'http://localhost'
-export const API_PORT = '1337'
-export const API_GATEWAY = `${API_URL}:${API_PORT}`
+export const API_GATEWAY = import.meta.env.BACKEND_URL;
 export const COLORS = {
-  black: '#000000',
-  white: '#ffffff',
-  redLight: '€C95370',
-  redDark: '#BD2753',
-  blueLight: '#517186',
-  blueDark: '#294B62',
-  purpleLight: '#7773CB',
-  purpleDark: '#54509D',
-}
+  black: "#000000",
+  white: "#ffffff",
+  redLight: "€C95370",
+  redDark: "#BD2753",
+  blueLight: "#517186",
+  blueDark: "#294B62",
+  purpleLight: "#7773CB",
+  purpleDark: "#54509D",
+};
