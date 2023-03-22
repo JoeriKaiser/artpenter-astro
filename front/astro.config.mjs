79765688c84c190f9ae1e58fa7f81cb8c3190539
JoeriKaiser <joeri.kaiser@hotmail.com>
2023-03-22 20:19:11 +0100

    static

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -2,16 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
-// import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 
 import react from "@astrojs/react";
-<<<<<<< HEAD
-import node from "@astrojs/node";
-
-=======
-import vercel from "@astrojs/vercel/serverless";
->>>>>>> ea1bf032ae059cc2e0377ca49ff7964aeb8fa601
+import vercel from "@astrojs/vercel/static";
 // https://astro.build/config
 export default defineConfig({
   site: "https://lea-stauder.art/",
@@ -25,19 +20,15 @@ export default defineConfig({
       smartypants: true,
       gfm: true,
     }),
-<<<<<<< HEAD
-    sitemap(),
-=======
-    // sitemap({
-    //   customPages: [
-    //     "https://lea-stauder.art",
-    //     "https://lea-stauder.art/contact",
-    //   ],
-    // }),
->>>>>>> ea1bf032ae059cc2e0377ca49ff7964aeb8fa601
+    sitemap({
+      customPages: [
+        "https://lea-stauder.art",
+        "https://lea-stauder.art/contact",
+      ],
+    }),
     svelte(),
     react(),
   ],
-  output: "server",
-  adapter: node({ mode: "standalone" }),
+  output: "static",
+  adapter: vercel(),
 });
