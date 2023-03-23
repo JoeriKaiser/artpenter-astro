0d306fe6e16f69faba3e9b9b24f3741627e15af7
JoeriKaiser <joeri.kaiser@hotmail.com>
2023-03-23 23:26:34 +0100

    removed unnecessary stuff

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,29 +1,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import svelte from "@astrojs/svelte";
 
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
+
 export default defineConfig({
   site: "https://lea-stauder.art/",
   integrations: [
     tailwind(),
+    react(),
     mdx({
       extendMarkdownConfig: false,
       smartypants: true,
       gfm: true,
     }),
-    sitemap({
-      customPages: [
-        "https://lea-stauder.art",
-        "https://lea-stauder.art/contact",
-      ],
-    }),
-    svelte(),
-    react(),
   ],
   output: "server",
   adapter: vercel(),
--- front/src/assets/brushData.js
+++
@@ -1,4 +0,0 @@
-export const brushData = {
-  path1: "Il ne s'agit pas de peindre",
-  path2: "Il ne s'agit pas de peindre",
-}
