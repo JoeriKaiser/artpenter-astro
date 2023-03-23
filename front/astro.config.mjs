ce7347472d864eb4060771c321b25caab0804103
JoeriKaiser <joeri.kaiser@hotmail.com>
2023-03-23 23:06:25 +0100

    build and removed packages

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
@@ -12,9 +11,6 @@ export default defineConfig({
   site: "https://lea-stauder.art/",
   integrations: [
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     mdx({
       extendMarkdownConfig: false,
       smartypants: true,
