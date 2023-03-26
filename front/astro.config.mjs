1521185c848b4b18cbd3933640de900eb0e05709
Joeri Kaiser <joeri.kaiser@medelse.com>
2023-03-27 00:00:32 +0200

    deno deploy test

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,22 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
-// https://astro.build/config
+import deno from "@astrojs/deno";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://lea-stauder.art/",
-  integrations: [
-    tailwind(),
-    react(),
-    mdx({
-      extendMarkdownConfig: false,
-      smartypants: true,
-      gfm: true,
-    }),
-  ],
+  integrations: [tailwind(), react(), mdx({
+    extendMarkdownConfig: false,
+    smartypants: true,
+    gfm: true
+  })],
   output: "server",
-  adapter: vercel(),
+  adapter: deno()
 });
