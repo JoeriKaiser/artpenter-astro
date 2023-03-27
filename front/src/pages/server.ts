e104dfd178033c8f03b2c9ebe2cf293f8b1587da
Joeri Kaiser <joeri.kaiser@medelse.com>
2023-03-27 11:55:11 +0200

    typo

--- front/src/pages/server.ts
+++ front/src/pages/server.ts
@@ -1,6 +1,6 @@
 console.log(`process running in '${Deno.cwd()}'`)
 console.log(`impor t.meta.env.MY_VAR = ${import.meta.env.PUBLIC_BACKEND_URL}`)
-console.log(`Deno.env.get("MY_VAR") = ${Deno.env.get("MY_VAR")}`)
+console.log(`Deno.env.get("MY_VAR") = ${Deno.env.get("PUBLIC_BACKEND_URL")}`)
 
 function get_var(){
     return Deno.env.get("PUBLIC_BACKEND_URL")
